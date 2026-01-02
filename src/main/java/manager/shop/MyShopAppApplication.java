351bb6ae26455ab6143a29b284fdeaa3c78d2db3
antonmakcovskiy <antonmakho@gmail.com>
2026-01-02 16:54:49 +0100

    create project

---
+++ src/main/java/manager/shop/MyShopAppApplication.java
@@ -0,0 +1,13 @@
+package manager.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopAppApplication.class, args);
+	}
+
+}
